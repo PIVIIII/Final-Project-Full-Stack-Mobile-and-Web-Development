fe368bd5a831204e8c484130e8d423f53c68cec1
Variya Iyara <lenvariya@gmail.com>
2026-03-14 14:52:30 +0700

    add /task

--- server/server.js
+++ server/server.js
@@ -14,6 +14,7 @@ import reviewRoutes from './routes/reviewRoutes.js';
 dotenv.config();
 
 const app = express();
+const ipTracker = {};
 
 app.use(morgan('dev'));
 
@@ -67,6 +68,52 @@ app.get('/health', (req, res) => {
     memory_usage_mb: memoryMB,
     timestamp: new Date().toISOString(),
   });
+});
+
+app.get('/task', (req, res) => {
+  const clientIp = req.ip;
+
+  const now = Date.now();
+  const WINDOW_TIME = 10000;
+  const MAX_REQUEST = 5;
+
+  if (!ipTracker[clientIp]) {
+    ipTracker[clientIp] = {
+      count: 1,
+      startTime: now,
+    };
+  } else {
+    const elapsed = now - ipTracker[clientIp].startTime;
+
+    if (elapsed > WINDOW_TIME) {
+      ipTracker[clientIp] = {
+        count: 1,
+        startTime: now,
+      };
+    } else {
+      ipTracker[clientIp].count += 1;
+    }
+  }
+
+  const data = ipTracker[clientIp];
+
+  if (data.count > MAX_REQUEST) {
+    const remaining = Math.ceil((WINDOW_TIME - (now - data.startTime)) / 1000);
+
+    return res.status(429).json({
+      error: 'Too many requests',
+      retry_after: remaining,
+    });
+  }
+
+  res.json({
+    message: 'Task completed successfully!',
+    request_count: data.count,
+  });
+
+  setTimeout(() => {
+    delete ipTracker[clientIp];
+  }, WINDOW_TIME);
 });
 app.listen(process.env.PORT, () => {
   console.log('Identity Scanner Server is running on http://localhost:8080');
